dd3445580c42c30e9c991d08a5d488718e9115ed
lloydagola <lloydagola@gmail.com>
2019-10-17 10:29:39 +0300

    Used the 'cors' library to enable cross-origin requests

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require("cors");
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -19,6 +20,7 @@ mongooseConnection.once("connected", () => console.log("Database connected..."))
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
